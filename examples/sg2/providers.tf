2c0d4711201947cdf0bd346af45022b4a45316b2
alibek17 <alybekkalyk@gmail.com>
2021-01-27 21:21:36 -0500

    other examples

---
+++ examples/sg2/providers.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = "~> 0.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  # shared_credentials_file = "/Users/aisuluuomokoeva/.aws/credentials"
+  # profile = "default"
+}
+
+resource "aws_security_group" "first_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
