cedfe05d6ef0e15cce07e91805ca1c905df305aa
alibek17 <alybekkalyk@gmail.com>
2021-03-01 20:05:29 -0500

    modlue change, action added

--- Session_09/main.tf
+++ Session_09/main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source = "../modules/s3"
+  source = "github.com/aKumoSolutions/terraform-january-2021//modules/s3"
   s3_bucket_name = var.s3_name
   versioning_enabled = var.is_versioning_enabled
   principals = var.principals_list
